b73c592affc6ced38fbc169c46618e68c044ffd7
Myari1 <myari@csus.edu>
2023-03-13 16:29:10 -0700

    mursal

--- Hello.java
+++ Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("303068428 - This line was added by Mursal Yari.");
+		System.out.println("303068428 - This line was added by Mursal Yari. ");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
