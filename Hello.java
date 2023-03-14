c4d76406df36198e42d75b60556729c1e006389c
mrb1ue <3781565-mrb1u@users.noreply.gitlab.com>
2023-03-13 17:36:39 -0700

    Merge branch 'main' of https://github.com/LJJones123/CSC-131.git into main

--- Hello.java
+++ Hello.java
@@ -6,17 +6,12 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by First name Last name.");
-<<<<<<< HEAD
-		System.out.println("301800915 - This line was added by Lydell Jones!.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-=======
-		System.out.println("302832790 - This line was added by Julian Flores");
 		System.out.println("220168755 - This line was added by Jeff Huang.");
+		System.out.println("301800915 - This line was added by Lydell Jones!.");
+		System.out.println("302832790 - This line was added by Julian Flores");
 		System.out.println("303119440 - This line was added by Salvador Del Rio Torres!!");
 		System.out.println("303068428 - This line was added by Mursal Yari. ");
 		System.out.println("303040296 - This line was added by Mayi Ananda!");
->>>>>>> branch 'main' of https://github.com/LJJones123/CSC-131.git
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
